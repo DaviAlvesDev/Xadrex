69614a8acfe1e2de6987eb2648cec7cd312317d0
Davi Alves <252664395+DaviAlvesDev@users.noreply.github.com>
2026-05-02 11:05:09 -0300

    Mobilidade das peças

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
+#include <iostream>
 #include <raylib.h>
 #include "config.hpp"
+
+struct PosicaoCasa
+{
+    uint8 coluna = 'a';
+    uint8 linha = '1';
+
+    bool operator==(PosicaoCasa a)
+    {
+        return this->coluna == a.coluna && this->linha == a.linha;
+    }
+};
 
 struct Peca
 {
     bool is_branca = true;
     uint8 tipo = PEAO;
     Vector2  pos = {0, 0};
-    uint8 coluna = 'a';
-    uint8 linha = '1';
+    PosicaoCasa pos_casa = {};
     Texture2D textura;
+    bool is_selecionada = false;
 };
 
 struct Casa 
 {
-    uint8 coluna = 'a';
-    uint8 linha = '1';
+    PosicaoCasa pos_casa = {};
     Vector2 pos = {0, 0};
     Color cor = WHITE;
 };
@@ -22,6 +33,7 @@ struct Casa
 Peca pecas[32];
 Casa tabuleiro[8][8];
 uint8 tipo_pecas[] = {TORRE, CAVALO, BISPO, DAMA, REI, PEAO};
+bool is_peca_selecionada = false;
 
 void  desenhar_tabuleiro();
 void desenhar_pecas();
@@ -29,6 +41,11 @@ void posicionar_pecas();
 void criar_pecas();
 void criar_tabuleiro();
 Texture2D carregar_textura(uint8 tipo);
+void descarregar();
+PosicaoCasa posicao_mouse();
+void lidar_com_click();
+void selecionar_peca(PosicaoCasa casa);
+void mover_peca(Peca &peca, PosicaoCasa casa);
 
 int main()
 {
@@ -46,10 +63,98 @@ int main()
         desenhar_tabuleiro();
         desenhar_pecas();
 
+        lidar_com_click();
+
         EndDrawing();
     }
 
+    descarregar();
+
     CloseWindow();
+}
+
+PosicaoCasa posicao_mouse()
+{
+    Vector2 mouse = GetMousePosition();
+
+    uint8 coluna = (mouse.x - offset_x) / CASA_DIMEN;
+    uint8 linha = (mouse.y - offset_y) / CASA_DIMEN;
+
+    PosicaoCasa pos_mouse;
+    pos_mouse.coluna = coluna + 'a';
+    pos_mouse.linha = '8' - linha;
+
+    return pos_mouse;
+}
+
+void selecionar_peca(PosicaoCasa casa)
+{
+    for (int i = 0; i  < 32; i++)
+    {
+        TraceLog(LOG_INFO, "Comparando click %c%c com peca em %c%c", casa.coluna, casa.linha, pecas[i].pos_casa.coluna, pecas[i].pos_casa.linha);
+
+        if (pecas[i].pos_casa == casa) 
+        {
+            pecas[i].is_selecionada = true;
+            is_peca_selecionada = true;
+            return;
+        }
+    }
+}
+
+void mover_peca(Peca &peca, PosicaoCasa casa)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (tabuleiro[i][j].pos_casa == casa)
+            {
+                peca.pos.x = tabuleiro[i][j].pos.x + PECA_OFFSET;
+                peca.pos.y = tabuleiro[i][j].pos.y + PECA_OFFSET;
+                
+                peca.pos_casa = casa; 
+                
+                peca.is_selecionada = false;
+                is_peca_selecionada = false;
+                return;
+            }
+        }
+    }
+}
+
+void lidar_com_click()
+{
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    {
+        PosicaoCasa mouse = posicao_mouse();
+
+        if (mouse.coluna >= 'a' && mouse.coluna <= 'h' && mouse.linha >= '1' && mouse.linha <= '8')
+        {
+            TraceLog(LOG_INFO, "Casa: %c%c", mouse.coluna, mouse.linha);
+
+            if (!is_peca_selecionada) selecionar_peca(mouse);
+            else 
+            {
+                for (int i = 0; i < 32; i++)
+                {
+                    if (pecas[i].is_selecionada) 
+                    {
+                        mover_peca(pecas[i], mouse);
+                        break;
+                    }
+                }
+            } 
+        }
+    }
+}
+
+void descarregar()
+{
+    for (int i = 0; i < 32; i++)
+    {
+        UnloadTexture(pecas[i].textura);
+    }
 }
 
 Texture2D carregar_textura(uint8 tipo)
@@ -107,32 +212,28 @@ void  desenhar_tabuleiro()
 
 void criar_tabuleiro()
 {
-    uint32 x = offset_x;
-    uint32 y = offset_y;
-    uint8 c = 'h';
-    uint8 l = '1';
-    bool branco = false;
-    for (int i = 7; i >= 0; i--)
+    float x = offset_x;
+    float y = offset_y;
+
+    for (int i = 0; i < 8; i++) // Linhas (0 a 7)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < 8; j++) // Colunas (0 a 7)
         {
-            branco = !branco;
-            Color cor;
+            // Define a cor alternada
+            Color cor = ((i + j) % 2 == 0) ? casaBranca : casaPreta;
 
-            if (branco) cor = casaBranca;
-            else cor = casaPreta;
+            // Coordenadas lógicas:
+            // Coluna: 'a' + j (0='a', 1='b'...)
+            // Linha:  '8' - i (0='8', 1='7'...) -> Para o topo ser a linha 8
+            uint8 col_char = 'a' + j;
+            uint8 lin_char = '8' - i;
 
-            uint8 coluna = c + 'a';
-            uint8 linha = l + '1';
+            tabuleiro[j][i].pos_casa = {col_char, lin_char};
+            tabuleiro[j][i].pos = { x, y };
+            tabuleiro[j][i].cor = cor;
 
-            tabuleiro[j][i] = {coluna, linha, {(float) x, (float) y}, cor};
-            c--;
             x += CASA_DIMEN;
         }
-        c = 'h';
-        l++;
-
-        branco = !branco;
         x = offset_x;
         y += CASA_DIMEN;
     }
@@ -146,45 +247,47 @@ void criar_pecas()
         {
             for (int j = 0; j < 8; j++)
             {
-                pecas[j+8] = {true, PEAO, {}, 0, 0, carregar_textura(PEAO)};
-                pecas[j+8+16] = {false, PEAO, {}, 0, 0, carregar_textura(PEAO)};
+                pecas[j+8] = {true, PEAO, {}, {}, carregar_textura(PEAO)};
+                pecas[j+8+16] = {false, PEAO, {}, {}, carregar_textura(PEAO)};
             }
             continue;
         }
 
         if (i > 2)
         {
-            pecas[i] = {true, tipo_pecas[i], {}, 0, 0, carregar_textura(tipo_pecas[i])};
-            pecas[i+16] = {false, tipo_pecas[i], {}, 0, 0, carregar_textura(tipo_pecas[i])};
+            pecas[i] = {true, tipo_pecas[i], {}, {}, carregar_textura(tipo_pecas[i])};
+            pecas[i+16] = {false, tipo_pecas[i], {}, {}, carregar_textura(tipo_pecas[i])};
             continue;
         }
 
-        pecas[i] = {true, tipo_pecas[i], {}, 0, 0, carregar_textura(tipo_pecas[i])};
-        pecas[7-i] = {true, tipo_pecas[i], {}, 0, 0, carregar_textura(tipo_pecas[i])};
+        pecas[i] = {true, tipo_pecas[i], {}, {}, carregar_textura(tipo_pecas[i])};
+        pecas[7-i] = {true, tipo_pecas[i], {}, {}, carregar_textura(tipo_pecas[i])};
 
-        pecas[i+16] = {false, tipo_pecas[i], {}, 0, 0, carregar_textura(tipo_pecas[i])};
-        pecas[7-i+16] = {false, tipo_pecas[i], {}, 0, 0, carregar_textura(tipo_pecas[i])};
+        pecas[i+16] = {false, tipo_pecas[i], {}, {}, carregar_textura(tipo_pecas[i])};
+        pecas[7-i+16] = {false, tipo_pecas[i], {}, {}, carregar_textura(tipo_pecas[i])};
     }
 }
 
 void posicionar_pecas()
 {
-    uint8 c = 0;
+    // Brancas nas linhas 0 e 1 do array (Linhas 8 e 7 do xadrez)
+    // Pretas nas linhas 7 e 6 do array (Linhas 1 e 2 do xadrez)
+    
     for (int i = 0; i < 32; i++)
     {
-        if (c > 7) c = 0;
+        int col_idx = i % 8;
+        int lin_idx;
 
-        uint8 l = pecas[i].is_branca ? 0 : 7;
-        if (i > 7 && i < 16) l++;
-        else if (i > 23) l--;
+        // Define a linha do array tabuleiro[col][lin] baseada no índice da peça
+        if (i < 8) lin_idx = 7;         // Brancas (Fundo)
+        else if (i < 16) lin_idx = 6;    // Peões Brancos
+        else if (i < 24) lin_idx = 0;    // Pretas (fundo)
+        else lin_idx = 1;               // Peões Pretos
 
-        pecas[i].pos.x = tabuleiro[c][l].pos.x + PECA_OFFSET;
-        pecas[i].pos.y = tabuleiro[c][l].pos.y + PECA_OFFSET;
-
-        pecas[i].coluna = tabuleiro[c][l].coluna;
-        pecas[i].linha = tabuleiro[c][l].linha;
-
-        c++;
+        // Copia do tabuleiro para a peça
+        pecas[i].pos.x = tabuleiro[col_idx][lin_idx].pos.x + PECA_OFFSET;
+        pecas[i].pos.y = tabuleiro[col_idx][lin_idx].pos.y + PECA_OFFSET;
+        pecas[i].pos_casa = tabuleiro[col_idx][lin_idx].pos_casa;
     }
 }
 
@@ -195,6 +298,10 @@ void desenhar_pecas()
         Color cor;
         if (pecas[i].is_branca) cor = corBrancas;
         else cor = corPretas;
+
+        if (pecas[i].is_selecionada) {
+            DrawCircleV({pecas[i].pos.x + PECA_DIMEN/2, pecas[i].pos.y + PECA_DIMEN/2}, 30, Fade(YELLOW, 0.6f));
+        }
 
         DrawTextureV(pecas[i].textura, pecas[i].pos, cor);
     }
